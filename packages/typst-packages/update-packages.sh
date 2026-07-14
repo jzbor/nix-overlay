8a47dda421fd39c4b05c28fea18766a5a412da6b
jzbor <jzbor@posteo.net>
2026-07-14 20:23:33 +0200

    Update typst packages

--- packages/typst-packages/update-packages.sh
+++ packages/typst-packages/update-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(echo "["; ls -1 $(nix build --no-link --print-out-paths .\#typstPackagesSrc)/packages/preview | sed 's/\(.*\)/  "\1"/'; echo "]") | tee package_names.nix
+(echo "["; ls -1 $(nix build --no-link --print-out-paths .\#typstPackagesSrc)/packages/preview | sed 's/\(.*\)/  "\1"/'; echo "]") > package_names.nix
